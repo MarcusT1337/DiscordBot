688fab708c2d0a4ee6e9d66c38d9ad4c954a5367
Marcus <marcy.toy@googlemail.com>
2025-07-14 11:06:44 +0100

    Initial Commit

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import os
 import discord
 from dotenv import load_dotenv
+
+#Aquiring discord token from the .env file
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client() #Loads the client
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord whoohoo!')
+
+client.run(TOKEN)
